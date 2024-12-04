3eafc7a7fa626593f00e66450f03ff91fc06a1ba
chefTee <taiwosharon36@gmail.com>
2024-12-04 08:28:05 +0100

    Added a feature

--- script.js
+++ script.js
@@ -3,13 +3,37 @@ const openBtn = document.querySelectorAll('.open-icon');
 const closeBtn = document.querySelectorAll('.close-icon');
 const answers = document.querySelectorAll('.answer');
 
+let selectedQuestion = null;
+
 questions.forEach((question, index) => {
     question.addEventListener('click', () => {
-        openBtn[index].classList.toggle("hide");
-        closeBtn[index].classList.toggle("hide");
-        answers[index].classList.toggle("close");
+
+        if (selectedQuestion !== null && selectedQuestion !== index){
+            openBtn[selectedQuestion].classList.remove('hide');
+            closeBtn[selectedQuestion].classList.add('hide');
+            answers[selectedQuestion].classList.add('close');
+        }
+
+        if(selectedQuestion === index) {
+            openBtn[index].classList.remove("hide");
+            closeBtn[index].classList.add("hide");
+            answers[index].classList.add("close");
+            question.setAttribute('aria-expanded', 'false');
+            selectedQuestion = null;
+        } else {
+            openBtn[index].classList.add("hide");
+            closeBtn[index].classList.remove("hide");
+            answers[index].classList.remove("close");
+            question.setAttribute('aria-expanded', 'true');
+            selectedQuestion = index;
+        }
+
+
+
+        selectedQuestion = answers[index].classList.contains('close') ? null : index;
 })
     });
+
     //Adding Keyboard Navigation
 questions.forEach((question, index) => {
     question.setAttribute('tabindex', index === 0 ? '0' : '-1')
@@ -47,7 +71,10 @@ questions.forEach((question, index) => {
     question.setAttribute('aria-expanded', 'false');
     question.setAttribute('aria-controls', `answer-${index}`);
     answers[index].setAttribute('id', `answer-${index}`);
+    questions[selectedQuestion].setAttribute('aria-expanded', 'false');
 })
+
+
 
 
 
